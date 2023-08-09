a23af82182e857628b4673e6a3f0b7f761c167df
Alexis Perez <45041860+Alecxz20@users.noreply.github.com>
2023-08-09 06:56:43 -0500

    add database logic and classes constructors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,72 @@
 import './index.css';
 
-const ham = document.querySelector('.ham');
-const menu = document.querySelector('.sidebarMenu');
+class Workspace {
+  constructor(name) {
+    this._name = name;
+  }
+  getName() {
+    return this._name;
+  }
+  addTask(item) {
+    this.tasks.push(item);
+  }
+  tasks = [];
+}
 
-ham.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
+class Task {
+  constructor(name, date) {
+    this._name = name;
+    this._date = date;
+  }
+
+  getName() {
+    return this._name;
+  }
+  getDate() {
+    return this._date;
+  }
+}
+
+const database = (function () {
+  const _workspaceBoard = [];
+
+  function getWorkspaceBoard() {
+    return _workspaceBoard;
+  }
+
+  function addToWorkspaceBoard(...items) {
+    items.forEach((item) => _workspaceBoard.push(item));
+  }
+
+  function deleteItemWorkspace(element) {
+    const newArr = _workspaceBoard.filter(
+      (item) => item.getName() !== element.getName()
+    );
+    _workspaceBoard.splice(0, _workspaceBoard.length);
+    newArr.forEach((item) => _workspaceBoard.push(item));
+  }
+
+  (function initialState() {
+    const inbox = new Workspace('Inbox');
+    const work = new Workspace('Work');
+    const study = new Workspace('Study');
+    addToWorkspaceBoard(inbox, work, study);
+  })();
+
+  return {
+    deleteItemWorkspace,
+    addToWorkspaceBoard,
+    getWorkspaceBoard,
+  };
+})();
+
+const UI = (function () {
+  (function navFunctionality() {
+    const ham = document.querySelector('.ham');
+    const menu = document.querySelector('.sidebarMenu');
+
+    ham.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+  })();
+})();
